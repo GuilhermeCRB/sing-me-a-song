cc9702d7798a9d85bfa962a079799a12b0b1d516
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-08-01 12:53:42 -0300

    feat: tests if top recommendation page works properly

--- front-end/cypress/e2e/createRecommendation.cy.js
+++ front-end/cypress/e2e/createRecommendation.cy.js
@@ -34,15 +34,80 @@ describe("Recommendation tests:", () => {
             name: faker.unique(faker.animal.fish),
             youtubeLink: `https://www.youtube.com/watch?${faker.animal.fish()}`
         };
-    
+
         cy.visit(BASE_FRONT_URL);
-    
+
         cy.get("#name").type(recommendation.name);
         cy.get("#link").type(recommendation.youtubeLink);
-    
+
         cy.get("#send-button").click();
 
         cy.get("#upvote").click();
         cy.get("#score").should("contain.text", "1");
+    });
+
+    it("Dislikes a recommendation.", async () => {
+        const recommendation = {
+            name: faker.unique(faker.animal.fish),
+            youtubeLink: `https://www.youtube.com/watch?${faker.animal.fish()}`
+        };
+
+        cy.visit(BASE_FRONT_URL);
+
+        cy.get("#name").type(recommendation.name);
+        cy.get("#link").type(recommendation.youtubeLink);
+
+        cy.get("#send-button").click();
+
+        cy.get("#downvote").click();
+        cy.get("#score").should("contain.text", "-1");
+    });
+
+    it("Navigates to top page and back to home page.", async () => {
+        const recommendation1 = {
+            name: faker.unique(faker.animal.fish),
+            youtubeLink: `https://www.youtube.com/watch?${faker.animal.fish()}`
+        };
+
+        const recommendation2 = {
+            name: faker.unique(faker.animal.fish),
+            youtubeLink: `https://www.youtube.com/watch?${faker.animal.fish()}`
+        };
+
+        const recommendation3 = {
+            name: faker.unique(faker.animal.fish),
+            youtubeLink: `https://www.youtube.com/watch?${faker.animal.fish()}`
+        };
+
+        cy.visit(BASE_FRONT_URL);
+
+        cy.get("#name").type(recommendation1.name);
+        cy.get("#link").type(recommendation1.youtubeLink);
+
+        cy.get("#send-button").click();
+
+
+        cy.get("#name").type(recommendation2.name);
+        cy.get("#link").type(recommendation2.youtubeLink);
+
+        cy.get("#send-button").click();
+
+        cy.get("#name").type(recommendation3.name);
+        cy.get("#link").type(recommendation3.youtubeLink);
+
+        cy.get("#send-button").click();
+        
+        cy.get("article:first #upvote").click();
+        cy.get("article").eq(1).get("#downvote").click();
+
+        cy.get("#top").click();
+        cy.url().should("equal", `${BASE_FRONT_URL}/top`);
+
+        cy.get("article").eq(0).get("#score").should("contain.text", "1");
+        cy.get("article").eq(1).get("#score").should("contain.text", "0");
+        cy.get("article").eq(2).get("#score").should("contain.text", "-1");
+
+        cy.get("#home").click();
+        cy.url().should("equal", BASE_FRONT_URL);
     });
 });
